7ba601be80ae2d60362c04430224fd8e838ce5a4
Alex Cruz <du.alexcruz@gmail.com>
2018-01-24 18:03:30 +0100

    pnw:Make the Power menu configurable [2/2]

--- src/com/pnw/settings/fragments/PowerMenuSettings.java
+++ src/com/pnw/settings/fragments/PowerMenuSettings.java
@@ -16,66 +16,31 @@
 
 package com.pnw.settings.fragments;
 
-import android.content.Context;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.content.pm.UserInfo;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.os.UserManager;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+
 import com.android.settings.R;
-import android.support.annotation.NonNull;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.SettingsPreferenceFragment;
 
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.pnw.settings.preferences.Utils;
-
-public class PowerMenuSettings extends SettingsPreferenceFragment
-                implements Preference.OnPreferenceChangeListener {
-
-    private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
-
-    private SwitchPreference mPowermenuTorch;
+public class PowerMenuSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pnw_settings_powermenu);
-
-        final ContentResolver resolver = getActivity().getContentResolver();
-        final PreferenceScreen prefScreen = getPreferenceScreen();
-
-        mPowermenuTorch = (SwitchPreference) findPreference(KEY_POWERMENU_TORCH);
-        mPowermenuTorch.setOnPreferenceChangeListener(this);
-        if (!Utils.deviceSupportsFlashLight(getActivity())) {
-            prefScreen.removePreference(mPowermenuTorch);
-        } else {
-        mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
-                Settings.System.POWERMENU_TORCH, 0) == 1));
-        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mPowermenuTorch) {
-            boolean value = (Boolean) newValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.POWERMENU_TORCH, value ? 1 : 0);
-            return true;
-        }
         return false;
     }
 
@@ -83,5 +48,4 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.PNW_SETTINGS;
     }
-
 }
